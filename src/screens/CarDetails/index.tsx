d859bd5a3996e352b5c88261299ed3ea94c4fe25
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2024-07-31 06:37:17 -0300

    feat: details

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,5 +1,17 @@
 import BackButton from '@components/BackButton';
-import { Container, Header } from './styles';
+import {
+  Container,
+  About,
+  Header,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Price,
+  Period,
+  Rent,
+} from './styles';
 import ImageSlider from '@components/ImageSlider';
 
 const CarDetails = () => {
@@ -10,6 +22,23 @@ const CarDetails = () => {
           <BackButton onPress={() => {}} />
         </ImageSlider>
       </Header>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   );
 };
--- src/screens/CarDetails/styles.tsx
+++ src/screens/CarDetails/styles.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native';
 
 import { ThemeType } from '../../styles/theme';
-import { BorderlessButton } from 'react-native-gesture-handler';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
@@ -12,3 +12,61 @@ export const Container = styled.View`
 export const Header = styled.SafeAreaView`
   margin-top: 32px;
 `;
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    padding: 24,
+    alignItems: 'center',
+  },
+  showVerticalScrollIndicator: false,
+})``;
+
+export const Details = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 32px;
+`;
+
+export const Description = styled.View``;
+
+export const Brand = styled.Text`
+  font-family: ${({ theme }: { theme: ThemeType }) =>
+    theme.fonts.secondary_500};
+  font-size: ${RFValue(10)}px;
+  color: ${({ theme }: { theme: ThemeType }) => theme.colors.text_details};
+  text-transform: uppercase;
+`;
+
+export const Name = styled.Text`
+  font-family: ${({ theme }: { theme: ThemeType }) =>
+    theme.fonts.secondary_500};
+  font-size: ${RFValue(25)}px;
+  color: ${({ theme }: { theme: ThemeType }) => theme.colors.title};
+`;
+
+export const Price = styled.Text`
+  font-family: ${({ theme }: { theme: ThemeType }) =>
+    theme.fonts.secondary_500};
+  font-size: ${RFValue(25)}px;
+  color: ${({ theme }: { theme: ThemeType }) => theme.colors.main};
+`;
+
+export const Period = styled.Text`
+  font-family: ${({ theme }: { theme: ThemeType }) =>
+    theme.fonts.secondary_500};
+  font-size: ${RFValue(10)}px;
+  color: ${({ theme }: { theme: ThemeType }) => theme.colors.text_details};
+  text-transform: uppercase;
+`;
+
+export const About = styled.Text`
+  font-family: ${({ theme }: { theme: ThemeType }) => theme.fonts.primary_400};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }: { theme: ThemeType }) => theme.colors.text_details};
+  width: 100%;
+  margin-top: 24px;
+`;
+
+export const Rent = styled.View``;
